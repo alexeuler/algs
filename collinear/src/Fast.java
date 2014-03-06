8a2289e4c56656c2347fae4d92dcb6cded0af775
alex <akarasev@nrgc.com>
2014-03-06 17:51:04 +0400

    4th attempt

--- collinear/src/Fast.java
+++ collinear/src/Fast.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -7,32 +8,60 @@ public class Fast {
 
     private static Point[] data;
     private static Point[] sorted_data;
-    private static Node printed;
+    private static List<List<Point>> lines = new List<List<Point>>();
 
-    private static class Node {
-        private Node next;
-        private Node last;
-        private String data;
+    private static class List<Item extends Comparable<Item>> implements Comparable<List<Item>> {
+        public Object[] data;
+        public int size;
 
-        public Node(String init) {
-            this.data = init;
-            this.last = this;
+        public List() {
+            data = new Object[4];
+            size = 0;
         }
 
-        public void push(String item) {
-            last.next = new Node(item);
-            last = last.next;
+        public Item get(int i) {
+            return (Item) data[i];
         }
 
-        public boolean has(String item) {
-            Node pointer = this;
-            while (pointer != null) {
-                if (pointer.data.equals(item)) {
-                    return true;
-                }
-                pointer = pointer.next;
+        public void push(Item item) {
+            if (size == data.length) {
+                resize(2 * data.length);
             }
-            return false;
+            data[size++] = item;
+        }
+
+        public int compareTo(List<Item> that) {
+            if (this.size < that.size) {
+                return -1;
+            }
+            if (this.size > that.size) {
+                return 1;
+            }
+            for (int i = 0; i < this.size; i++) {
+                Item thisItem = this.get(i);
+                Item thatItem = that.get(i);
+                if (thisItem.compareTo(thatItem) == 0) {
+                    continue;
+                }
+                return thisItem.compareTo(thatItem);
+            }
+            return 0;
+        }
+
+        public void sort() {
+            resize(size);
+            Arrays.sort(data);
+        }
+
+        public <T> T[] toArray(T[] a) {
+            return (T[]) Arrays.copyOf(data, size, a.getClass());
+        }
+
+        private void resize(int capacity) {
+            Object[] copy = new Object[capacity];
+            for (int i = 0; i < size; i++)
+                copy[i] = data[i];
+            data = copy;
         }
     }
 
@@ -52,61 +81,61 @@ public class Fast {
     }
 
     private static void findAndPrint() {
-        printed = new Node("");
         for (Point p : data) {
             Arrays.sort(sorted_data, p.SLOPE_ORDER);
-            printEqual(p);
+            findCollinear(p);
         }
+        printLines();
     }
 
-    private static void printEqual(Point p) {
+    private static void findCollinear(Point p) {
         int n = 1;
         double slope = p.slopeTo(sorted_data[0]);
-        for (int i = 1; i < sorted_data.length; i++) {
-            if (p.slopeTo(sorted_data[i]) == slope) {
+        for (int i = 1; i < sorted_data.length + 1; i++) {
+            if ((i != sorted_data.length) && (p.slopeTo(sorted_data[i]) == slope)) {
                 n++;
                 continue;
             } else {
                 if (n >= 3) {
-                    Point[] collinear = new Point[n + 1];
+                    List<Point> collinear = new List<Point>();
                     for (int j = 0; j < n; j++) {
-                        collinear[j] = sorted_data[i - 1 - j];
+                        collinear.push(sorted_data[i - 1 - j]);
                     }
-                    collinear[n] = p;
-                    print(collinear);
+                    collinear.push(p);
+                    collinear.sort();
+                    lines.push(collinear);
                 }
                 n = 1;
-                slope = p.slopeTo(sorted_data[i]);
+                if (i != sorted_data.length) {
+                    slope = p.slopeTo(sorted_data[i]);
+                }
+                ;
             }
         }
-        if (n >= 3) {
-            Point[] collinear = new Point[n + 1];
-            for (int j = 0; j < n; j++) {
-                collinear[j] = sorted_data[sorted_data.length - 1 - j];
-            }
-            collinear[n] = p;
-            print(collinear);
-        }
-
     }
 
-    private static void print(Point[] points) {
-        int i = 0;
-        Arrays.sort(points);
-
-        String result = "";
-        for (Point p : points) {
-            i++;
-            if (i != points.length) {
-                result += p.toString() + " -> ";
+    private static void printLines() {
+        lines.sort();
+        List<Point> a = lines.get(0);
+        printLine(a);
+        for (int i = 1; i < lines.data.length; i++) {
+            if (lines.get(i).compareTo(lines.get(i - 1)) == 0) {
+                continue;
             }
+            printLine(lines.get(i));
         }
-        result += points[points.length - 1].toString() + "\n";
-        if (!printed.has(result)) {
-            StdOut.print(result);
-            points[0].drawTo(points[points.length - 1]);
-            printed.push(result);
+    }
+
+    private static void printLine(List<Point> list) {
+        Point[] array = list.toArray(new Point[0]);
+        String result = "";
+        result += array[0].toString();
+        array[0].drawTo(array[array.length - 1]);
+        for (int i = 1; i < array.length; i++) {
+            result += " -> ";
+            result += array[i].toString();
         }
+        StdOut.println(result);
     }
 
     public static void main(String[] args) {
